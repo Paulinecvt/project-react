0206c8662dc90c91b5b0faa3ca1cc733c2fa509c
Pauline Cvt <paulinecvt2021@gmail.com>
2024-02-07 18:34:21 +0100

    modified logo, added logo for fronter and color palette

--- vite-project/src/components/Sidebar.jsx
+++ vite-project/src/components/Sidebar.jsx
@@ -1,11 +1,11 @@
 import React from "react"
+import { Link } from "react-router-dom";
 
 function Sidebar() {
     return (
         <div className="Sidebar">
-            <a href="https://github.com/Paulinecvt/project-react">Home</a>
-            <p> </p>
-            <a href="https://github.com/Paulinecvt/project-react">About</a>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
         </div>
     )
 }
